fca006eec6f7fc2914d6e8e60b331e5113935b5e
felipnunes <60801943+felipnunes@users.noreply.github.com>
2022-07-12 19:39:43 -0300

    Animação_misty_implementada(Parcialmente))
    Tem um bug nas animações de andar para a direita e esquerda.

---
+++ A Herdeira de Grinbell/Assets/Scenes/Scripts/Miaty_animation.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Miaty_animation : MonoBehaviour
+{
+    //SerializeFueld permite visualizar essa variavel privada no editor, permitindo que eu arraste um objeto para ele
+    [SerializeField]
+    private Animator animator;
+
+    [SerializeField]
+    public Rigidbody2D rb;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //testa se está no chão
+        if (rb.velocity.y == 0)
+        {
+            this.animator.SetBool("Esta_no_chao", true);
+        }
+        else
+        {
+            this.animator.SetBool("Esta_no_chao", false);
+        }
+
+        //testa se esta andando para a direita
+        if (rb.velocity.x > 0) 
+        {
+            this.animator.SetBool("Andando_direita",true);
+        }
+        else
+        {
+            this.animator.SetBool("Andando_direita", false);
+        }
+
+        //testa se esta andando para a esquerda
+        if (rb.velocity.x < 0)
+        {
+            this.animator.SetBool("Andando_esquerda", true);
+        }
+        else
+        {
+            this.animator.SetBool("Andando_esquerda", false);
+        }
+
+        //testa se esta pulando para a direita
+        if (rb.velocity.y != 0 && rb.velocity.x > 0)
+        {
+            this.animator.SetBool("Pulando_direita", true);
+        }
+        else
+        {
+            this.animator.SetBool("Pulando_direita", false);
+        }
+    }
+}
