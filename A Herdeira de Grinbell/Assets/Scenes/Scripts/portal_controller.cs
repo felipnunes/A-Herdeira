94462ca4522f71988c64584c19debe428084e437
felipnunes <60801943+felipnunes@users.noreply.github.com>
2022-07-14 15:04:35 -0300

    Passar_de_fase_implementado
    Portal funcionando e adição da Misty no menu.

---
+++ A Herdeira de Grinbell/Assets/Scenes/Scripts/portal_controller.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class portal_controller : MonoBehaviour
+{
+    public void Carrega_cenas(string cena)
+    {
+        SceneManager.LoadScene(cena);
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Misty")
+        {
+            Carrega_cenas("Fase_2");
+        }
+    }
+}
