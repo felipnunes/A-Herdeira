b21c93571dfa58124b932579888d7e2feaf44e1d
felipnunes <60801943+felipnunes@users.noreply.github.com>
2022-07-14 12:16:03 -0300

    Menu_prototipo
    Protótipo de menu implementado e funcionando corretamente.

---
+++ A Herdeira de Grinbell/Assets/Scenes/Scripts/Menu_controller.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu_controller : MonoBehaviour
+{
+    public void Carrega_cenas(string cena) 
+    {
+        SceneManager.LoadScene(cena);
+    }
+
+    public void Sair()
+    {
+        Application.Quit();
+    }
+}
