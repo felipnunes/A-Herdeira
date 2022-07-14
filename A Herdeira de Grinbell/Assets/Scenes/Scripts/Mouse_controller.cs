a7d6f9c33cb48e7c1cfe8ca82b55aead716021f0
felipnunes <60801943+felipnunes@users.noreply.github.com>
2022-07-14 19:34:02 -0300

    Icone_mouse_Completo
    Implementadas as mudanças de cor ao clicar.

--- A Herdeira de Grinbell/Assets/Scenes/Scripts/Mouse_controller.cs
+++ A Herdeira de Grinbell/Assets/Scenes/Scripts/Mouse_controller.cs
@@ -4,11 +4,15 @@ using UnityEngine;
 
 public class Mouse_controller : MonoBehaviour
 {
+    Sprite neutro;
+    public Sprite rosa;
+    public Sprite vermelho;
 
     public Transform pointer;
     // Start is called before the first frame update
     void Start()
     {
+        neutro = this.GetComponent<SpriteRenderer>().sprite;
         Cursor.visible = false;
     }
 
@@ -19,5 +23,18 @@ public class Mouse_controller : MonoBehaviour
         mouse_position.z = 0.5f;
 
         pointer.position = Camera.main.ScreenToWorldPoint(mouse_position);
+
+        if (Input.GetMouseButton(0))
+        {
+            this.GetComponent<SpriteRenderer>().sprite = rosa;
+        }
+        else if (Input.GetMouseButton(1))
+        {
+            this.GetComponent<SpriteRenderer>().sprite = vermelho;
+        }
+        else
+        {
+            this.GetComponent<SpriteRenderer>().sprite = neutro;
+        }
     }
 }
