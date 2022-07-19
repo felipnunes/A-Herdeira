5543f5c793d94509a6c2e15360282ec73c6da6cf
felipnunes <60801943+felipnunes@users.noreply.github.com>
2022-07-19 17:50:35 -0300

    Botao_menu_implementado

--- A Herdeira de Grinbell/Assets/Scenes/Scripts/Pause_screen.cs
+++ A Herdeira de Grinbell/Assets/Scenes/Scripts/Pause_screen.cs
@@ -1,15 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Pause_screen : MonoBehaviour
 {
     private GameObject Pause_canvas;
+    private GameObject botão_menu;
     private bool pausado;
 
     private void Start()
     {
         Pause_canvas = GameObject.Find("Pause_panel");
+        botão_menu = GameObject.Find("Menu_button");
         Pause_canvas.SetActive(false);
     }
     void Update()
@@ -27,6 +30,12 @@ public class Pause_screen : MonoBehaviour
             Time.timeScale = 1f;
             Pause_canvas.SetActive(false);
 
-        }
+        }   
     }
+
+    public void Carrega_menu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+    
 }
