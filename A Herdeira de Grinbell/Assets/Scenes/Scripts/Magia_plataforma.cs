06aa2845574fead51d99d9dd76da19208a538170
felipnunes <60801943+felipnunes@users.noreply.github.com>
2022-07-21 13:34:22 -0300

    Orbes_implementadas
    Dinamicas de orbes completamente implementadas.

--- A Herdeira de Grinbell/Assets/Scenes/Scripts/Magia_plataforma.cs
+++ A Herdeira de Grinbell/Assets/Scenes/Scripts/Magia_plataforma.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 
 public class Magia_plataforma : MonoBehaviour
 {
-    bool esta_sendo_segurado = false;
+    private bool esta_sendo_segurado = false;
+    private bool misty_colidindo = false;
     [SerializeField] GameObject misty;
+   
 
     // Update is called once per frame
     void Update()
@@ -13,11 +15,11 @@ public class Magia_plataforma : MonoBehaviour
         Vector2 mouse_position = Input.mousePosition;
         mouse_position = Camera.main.ScreenToWorldPoint(mouse_position);
 
-        if (esta_sendo_segurado && this.name == "Plataforma_rosa_vertical")
+        if (esta_sendo_segurado && this.name == "Plataforma_rosa_vertical" && misty_colidindo == false)
         {
             this.gameObject.transform.localPosition = new Vector3(this.gameObject.transform.localPosition.x, mouse_position.y, 0);
         }
-        if (esta_sendo_segurado && this.name == "Plataforma_rosa_horizontal")
+        if (esta_sendo_segurado && this.name == "Plataforma_rosa_horizontal" && misty_colidindo == false)
         {
             this.gameObject.transform.localPosition = new Vector3(mouse_position.x, this.gameObject.transform.localPosition.y, 0);
         }
@@ -25,7 +27,7 @@ public class Magia_plataforma : MonoBehaviour
 
     private void OnMouseDown()
     {
-        if (Input.GetMouseButton(0) && misty.GetComponent<Misty_controller>().quantidade_magias_rosa > 0)
+        if (Input.GetMouseButton(0) && misty.GetComponent<Misty_controller>().quantidade_magias_rosa > 0 && misty_colidindo == false)
         {
             Vector2 mouse_position = Input.mousePosition;
             mouse_position = Camera.main.ScreenToWorldPoint(mouse_position);
@@ -39,5 +41,21 @@ public class Magia_plataforma : MonoBehaviour
     private void OnMouseUp()
     {
         esta_sendo_segurado = false;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Misty")
+        {
+            misty_colidindo = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Misty")
+        {
+            misty_colidindo = false;
+        }
     }
 }
---
+++ A Herdeira de Grinbell/Assets/Scenes/Scripts/Orb_controller.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Orb_controller : MonoBehaviour
+{
+   [SerializeField] GameObject misty;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Misty" && this.tag == "Orbe_rosa")
+        {
+            Destroy(this.gameObject);
+            misty.GetComponent<Misty_controller>().quantidade_magias_rosa += 1;
+        }
+
+        if (collision.tag == "Misty" && this.tag == "Orbe_vermelha")
+        {
+            Destroy(this.gameObject);
+            misty.GetComponent<Misty_controller>().quantidade_magias_vermelha += 1;
+        }
+    }
+}
