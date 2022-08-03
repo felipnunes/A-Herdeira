a68d7f7754d2749d79cd7cc1b15c288152fa69e0
felipnunes <60801943+felipnunes@users.noreply.github.com>
2022-08-02 21:57:56 -0300

    animação_cutcene_sincronizada_parcialmente
    O texto está sincronizado com a animação até a metade. falta fazer os desenhos finais da cutsene e sincronizar a segunda metade.

--- A Herdeira de Grinbell/Assets/Scenes/Scripts/Texto_controller.cs
+++ A Herdeira de Grinbell/Assets/Scenes/Scripts/Texto_controller.cs
@@ -2,31 +2,56 @@
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Texto_controller : MonoBehaviour
-{
-   [SerializeField] private string[] textos;
-   AudioSource audio_source;
-   private Text texto_atual;
+    public class Texto_controller : MonoBehaviour
+    {
+        [SerializeField] private string[] textos;
+        AudioSource audio_source;
+        private Text texto_atual;
+        private bool esperando_input;
 
+
+    private void Start()
+    {
+        esperando_input = false;
+    }
     private void Awake()
-    {
-        audio_source = this.GetComponent<AudioSource>();
-        texto_atual = this.GetComponent<Text>();
-        StartCoroutine(InsereTexto(textos, texto_atual));
+        {
+            
+            audio_source = this.GetComponent<AudioSource>();
+            texto_atual = this.GetComponent<Text>();
+            StartCoroutine(InsereTexto(textos, texto_atual));
+        }
+
+        IEnumerator InsereTexto(string[] textos, Text texto_atual)
+        {
+            for (int i = 0; i < textos.Length; i++)
+            {
+                for (int j = 0; j < textos[i].Length; j++)
+                {
+                    
+                    texto_atual.text += textos[i][j];
+                    audio_source.Play();
+                    yield return new WaitForSeconds(0.05f);
+
+                    if (textos[i][j] == '.' && j != textos[i].Length - 1)
+                    {
+                        esperando_input = true;
+                        yield return new WaitUntil(() => Input.GetKeyDown(KeyCode.Space));
+                        esperando_input = false;
+                    }
+                }
+
+                esperando_input = true;
+                yield return new WaitUntil(() => Input.GetKeyDown(KeyCode.Space));
+                esperando_input = false;
+                texto_atual.text = "";
+            }
+        }
+
+        public bool GetEsperandoInput()
+        {
+            return esperando_input;
+        }
+
     }
 
-    IEnumerator InsereTexto(string[] textos, Text texto_atual)
-    {
-        for (int i = 0; i < textos.Length; i++)
-        {
-            for (int j = 0; j < textos[i].Length; j++)
-            {
-                texto_atual.text += textos[i][j];
-                audio_source.Play();
-                yield return new WaitForSeconds(0.05f);
-            }
-            yield return new WaitUntil(() => Input.GetKeyDown(KeyCode.Space));
-            texto_atual.text = "";
-        }
-    }
-}
---
+++ A Herdeira de Grinbell/Assets/Scenes/Scripts/cutscene_animator_controller.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class cutscene_animator_controller : MonoBehaviour
+{
+    [SerializeField] Animator texto_anim;
+    [SerializeField] Text texto;
+    void Start()
+    {
+        texto_anim.speed = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (texto.GetComponent<Texto_controller>().GetEsperandoInput())
+        {
+            texto_anim.speed = 0;
+        }
+        else
+        {
+            texto_anim.speed = 1;
+        }
+    }
+    
+}
