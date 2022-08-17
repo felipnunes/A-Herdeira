6af5e1be2eba6ea61b358b7cb728e9e3b2a0e69d
felipnunes <60801943+felipnunes@users.noreply.github.com>
2022-08-16 22:45:13 -0300

    Fase_11_e_sistema_de_mensagens_implementados
    Mensagens de instruções funcionando corretamente (falta o sprite do ícone). A fase 11 já está funcional e sem bugs.

---
+++ A Herdeira de Grinbell/Assets/Scenes/Scripts/Ajuda_icone_controller.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ajuda_icone_controller : MonoBehaviour
+{
+    [SerializeField] GameObject canvas;
+    // Start is called before the first frame update
+    void Start()
+    {
+     
+    }
+
+    // Update is called once per frame
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Misty")
+        {
+            canvas.SetActive(true);
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Misty")
+        {
+            canvas.SetActive(false);
+        }
+    }
+}
